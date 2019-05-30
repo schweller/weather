c67710e26646c5997914e9c489b80367b5e70309
Inácio Schweller <iscmendonca@gmail.com>
2019-05-29 21:22:51 -0300

    refactor on the weather controller and select components

--- src/App.js
+++ src/App.js
@@ -18,14 +18,18 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+const handleError = ({response}) => {
+  return {
+    error: response.data.error
+  }
+}
+
 const ShowWeather = ({data}) => {
-  if (!data) return null
-  const { name } = data
-  return (
-    <Typography variant="h4">
-      {`${data.weather[0].description} in ${name}`}
-    </Typography>
-  )
+  if (data && data.error) {
+    return <Typography variant="h4">{data.error}</Typography>
+  }
+
+  return <Typography variant="h4">{data}</Typography>
 }
 
 const useWeatherFetch = (country, subdivision) => {
@@ -33,8 +37,10 @@ const useWeatherFetch = (country, subdivision) => {
   useEffect(() => {
     if (subdivision) {
       const fetchWeather = async () => {
-        const weather = await axios(`http://localhost:9000/weather/${country}/${subdivision}`)
-        setWeather(weather.data)
+        const result = await axios(`http://localhost:9000/weather/${country}/${subdivision}`)
+          .then(response => response.data)
+          .catch(error => handleError(error))
+        setWeather(result)
       }
       fetchWeather()
     }
@@ -42,14 +48,45 @@ const useWeatherFetch = (country, subdivision) => {
   return weather
 }
 
-const Weather = () => {
+const SubdivisionsSelect = ({subdivision, country, inputLabel, labelWidth, handleChange}) => {
   const classes = useStyles()
-  const inputLabel = useRef(null)
-
-  const [countries, setCountries] = useState([])
-  const [country, setCountry] = useState('')
   const [subdivisions, setSubdivisions] = useState([])
-  const [subdivision, setSubdivision] = useState('')
+
+  useEffect(() => {
+    if (country !== '') {
+      const fetchSubdivisions = async () => {
+        const states = await axios(`http://localhost:9000/countries/subdivisions/${country}`)
+        setSubdivisions(states.data.RestResponse.result)
+      }
+      fetchSubdivisions()
+    }
+  }, [country])
+
+  return (
+    <FormControl variant="outlined" className={classes.formControl} disabled={subdivisions.length===0}>
+      <InputLabel ref={inputLabel} htmlFor="field-subdivisions">
+        Subdivisions
+      </InputLabel>
+      <NativeSelect
+        value={subdivision}
+        onChange={handleChange}
+        input={<OutlinedInput labelWidth={labelWidth} name="field-subdivisions" id="subdivisions" />}
+      >
+        <option value=""/>
+        {
+          subdivisions.length > 0 &&
+          subdivisions.map((sub, index) => {
+            return <option key={index} value={sub.capital}>{sub.name}</option>
+          })
+        }
+      </NativeSelect>
+    </FormControl> 
+  )
+}
+
+const CountriesSelect = ({country, inputLabel, labelWidth, handleChange}) => {
+  const classes = useStyles()
+  const [countries, setCountries] = useState([])
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -59,24 +96,39 @@ const Weather = () => {
     fetchCountries()
   }, [])
 
-  useEffect(() => {
-    if (country !== '') {
-      console.log(country)
-      const fetchSubdivisions = async () => {
-        const states = await axios(`http://localhost:9000/countries/subdivisions/${country}`)
-        console.log(states.data.RestResponse.result)
-        setSubdivisions(states.data.RestResponse.result)
-      }
-      fetchSubdivisions()
-    }
-  }, [country])
+  return (
+    <FormControl variant="outlined" className={classes.formControl}>
+      <InputLabel ref={inputLabel} htmlFor="field-country">
+        Country
+      </InputLabel>
+      <NativeSelect
+        value={country}
+        onChange={handleChange}
+        input={<OutlinedInput labelWidth={labelWidth} name="field-country" id="country" />}
+      >
+        <option value=""/>
+        {countries.length > 0 && 
+        countries.map((country, index) => {
+          return <option key={index} value={country.alpha3Code}>{country.name}</option>
+        })}
+      </NativeSelect>
+    </FormControl> 
+  )
+}
 
-  const weather = useWeatherFetch(country, subdivision)
+const Weather = () => {
+  const classes = useStyles()
+  const inputLabel = useRef(null)
 
   const [labelWidth, setLabelWidth] = useState(0)
-  React.useEffect(() => {
+  useEffect(() => {
     setLabelWidth(inputLabel.current.offsetWidth)
   }, [])
+
+  const [country, setCountry] = useState('')
+  const [subdivision, setSubdivision] = useState('')
+
+  const weather = useWeatherFetch(country, subdivision)
 
   const handleCountry = event => setCountry(event.target.value)
   const handleSubdivision = event => setSubdivision(event.target.value)
@@ -87,42 +139,21 @@ const Weather = () => {
       <Typography variant="h1" align="center">Weather</Typography>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <FormControl variant="outlined" className={classes.formControl}>
-            <InputLabel ref={inputLabel} htmlFor="field-country">
-              Country
-            </InputLabel>
-            <NativeSelect
-              value={country}
-              onChange={handleCountry}
-              input={<OutlinedInput labelWidth={labelWidth} name="field-country" id="country" />}
-            >
-              <option value=""/>
-              {countries.length > 0 && 
-               countries.map((country, index) => {
-                return <option key={index} value={country.alpha3Code}>{country.name}</option>
-              })}
-            </NativeSelect>
-          </FormControl>     
+          <CountriesSelect 
+            country={country} 
+            labelWidth={labelWidth} 
+            inputLabel={inputLabel}
+            handleChange={handleCountry}
+          />
         </Grid>
         <Grid item xs={12}>
-          <FormControl variant="outlined" className={classes.formControl} disabled={subdivisions.length===0}>
-            <InputLabel ref={inputLabel} htmlFor="field-subdivisions">
-              Subdivisions
-            </InputLabel>
-            <NativeSelect
-              value={subdivision}
-              onChange={handleSubdivision}
-              input={<OutlinedInput labelWidth={labelWidth} name="field-subdivisions" id="subdivisions" />}
-            >
-              <option value=""/>
-              {
-                subdivisions.length > 0 &&
-                subdivisions.map((sub, index) => {
-                  return <option key={index} value={sub.capital}>{sub.name}</option>
-                })
-              }
-            </NativeSelect>
-          </FormControl> 
+          <SubdivisionsSelect 
+            country={country} 
+            subdivision={subdivision}
+            labelWidth={labelWidth} 
+            inputLabel={inputLabel}
+            handleChange={handleSubdivision}
+          />
           <ShowWeather data={weather}/>
         </Grid>
       </Grid>
--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -1,12 +1,20 @@
 const axios = require('axios')
 
+const handleWeatherSuccess = (data) => {
+  return `${data.weather[0].description} in ${data.name}`
+}
+
 exports.getWeather = async (req, res) => {
   const { country, subdivision } = req.params
-  console.log(req.params)
-  const { data } =  await axios(
+  const response = await axios(
     `http://api.openweathermap.org/data/2.5/weather?q=${subdivision},${country}&APPID=eb212045c90a1d90a521b88ea995cc3a`
   )
-  console.log(data)
+    .then(result => handleWeatherSuccess(result.data))
+    .catch(error => error.response.status)
 
-  res.status(200).send(data)
+  if (response === 404) {
+    res.status(404).send({error: `No weather found for ${subdivision}`})
+  } else {
+    res.status(200).send(response)
+  }
 }
