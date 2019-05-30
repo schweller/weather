7bcb8b16b3b50f01454546523d2260cd281a7009
Inácio Schweller <iscmendonca@gmail.com>
2019-05-29 23:31:32 -0300

    bootstraping yarn for client and server apps

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = require('./app')
 const SERVERPORT = process.env.SERVERPORT
 
 const main = async () => {
-
   app.listen(SERVERPORT)
   console.log(`API Server - listening on port: ${SERVERPORT}`)
 }
